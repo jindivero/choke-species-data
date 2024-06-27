9e61c054710f97296f98cfd2822f89d3f1f1cdda
Tim Essington <tessington@users.noreply.github.com>
2024-06-27 16:04:03 -0700

    code to look at four years of data for comparison to newport

--- test_data/code/extract_glorys_all_years.R
+++ test_data/code/extract_glorys_all_years.R
@@ -22,7 +22,9 @@ nc_df <- nc %>%
   hyper_tibble %>%
   group_by(longitude, latitude) %>%
   filter(depth == max(depth)) %>%
-  ungroup()
+  ungroup() %>%
+  filter(depth >=30)
+
 # remove large list from memory
 rm(nc)
 
@@ -62,19 +64,29 @@ if (run_all_years) {
   setwd("/Users/essing/Library/CloudStorage/Dropbox/for_tim")
   
   files <- list.files("/Users/essing/Library/CloudStorage/Dropbox/for_tim")
-
-convert_glorys <- function(file_name) {
+  convert_glorys <- function(file_name) {
 
   nc <- tidync(file_name)
   nc_df <- nc %>%
     hyper_tibble %>%
     group_by(longitude, latitude) %>%
     filter(depth == max(depth)) %>%
-    ungroup()
+    ungroup() %>%
+    filter(depth >=30)
+  
   # remove large list from memory
   rm(nc)
+  days <- unique(nc_df$time)
+  n_days <- length(days)
+  first_day <- days[1]
+  last_day <- days[n_days]
+  days.2.use <- seq(first_day, last_day, by = 10)
+  nc_df <- nc_df %>%
+    filter(time %in% days.2.use)
+  
   
   nc_df$time <- (as_datetime("1950-01-01")+hours(nc_df$time))
+  
   nc_df <- nc_df %>%
     st_as_sf(coords=c('longitude','latitude'),crs=4326,remove = F) %>%  
     st_transform(crs = "+proj=utm +zone=10 +datum=WGS84 +units=km") %>% 
@@ -82,13 +94,19 @@ convert_glorys <- function(file_name) {
     st_set_geometry(NULL)
   nc_df$doy <- as.POSIXlt(nc_df$time, format = "%Y-%b-%d")$yday
   nc_df$year <- year(nc_df$time)
+  
   return(nc_df)
-}
+  }
+  yearly_glorys <- convert_glorys(files[1])
+  
+  for (i in 2:length(files)) {
+    tmp_glorys <- convert_glorys(files[i])
+    yearly_glorys <- rbind(yearly_glorys, tmp_glorys)
+  }
+  
+  }
 
-dats <- lapply(files, convert_glorys)
-}
-yearly_glorys <- do.call(data.frame, dats)
-rm(dats)
+rm(tmp_glorys)
 
 # load Newport data
 setwd(basewd)
@@ -112,12 +130,12 @@ newport_data <- newport_data %>%
 newport_data <- as.data.frame(newport_data)
 
 
-
+glory_years <- unique(yearly_glorys$year)
 # extract year and samples within time frame of model
 yearly_newport <- newport_data %>%
-  filter(year == 2019,
-         doy >= min(nc_df$doy),
-         doy <= max(nc_df$doy)
+  filter(year %in% glory_years,
+         doy >= min(yearly_glorys$doy),
+         doy <= max(yearly_glorys$doy)
   )
 source("test_data/code/convert_funs.R")
 yearly_newport$sigma <- calc_sigma( s = yearly_newport$practical.salinity,
@@ -130,18 +148,42 @@ yearly_newport$o2_fun <- convert_DO(x = yearly_newport$dissolved.oxygen..ml.L.,
                                     S = yearly_newport$practical.salinity,
                                     t = yearly_newport$temperature..degC.)
 # setup sdmTMB
-nc_df <- as.data.frame(nc_df)
-spde <- make_mesh(data = nc_df, xy_cols = c("X","Y"), n_knots = 250)
+yearly_glorys <- as.data.frame(yearly_glorys)
+spde <- make_mesh(data = yearly_glorys, xy_cols = c("X","Y"), n_knots = 250)
 m1 <- sdmTMB(formula = log(o2)  ~ 0 +  s(depth) + +s(doy)  ,
              mesh = spde,
-             data = nc_df, 
+             data = yearly_glorys, 
              family = gaussian(), 
-#             time = "doy",
+             time = "year",
              spatial = "on",
-             spatiotemporal  = "off")
+             spatiotemporal  = "iid")
 summary(m1)
 sanity(m1)
+### Plot marginal effect of depth ####
 
+depth_test <- tibble(X = yearly_newport$X[1],
+                     Y =  yearly_newport$Y[1],
+                     doy = mean(yearly_newport$doy),
+                     year = 2017,
+                     depth = seq(30,1300, by = 10))
+
+depth_predict <- predict(m1, newdata = depth_test)
+
+ggplot(depth_predict, aes(x = depth, y = exp(est))) + 
+         geom_line()
+xrange<- range(yearly_newport$X)
+yrange <- c(0.9, 1.1) * yearly_newport$Y[1]
+
+# plot yearly glorys near the newport line
+red_glorys <- yearly_glorys %>%
+  filter(X >= xrange[1], X <= xrange[2],
+         Y >= yrange[1], Y<= yrange[2])
+
+ggplot(red_glorys, aes(x = depth, y = o2, col = doy)) + 
+  geom_point()
+
+ggplot(yearly_newport, aes(x = depth, y = o2, col = doy)) + 
+  geom_point()
 newport_predict <- predict(m1, newdata = yearly_newport)
 
 
