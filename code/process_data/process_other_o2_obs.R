f109ec672be21a8f90ce4cb9b4097d7215e445dd
jindivero <84645567+jindivero@users.noreply.github.com>
2024-08-05 08:50:25 -0700

    add and process hake oxygen data

--- code/process_data/process_other_o2_obs.R
+++ code/process_data/process_other_o2_obs.R
@@ -614,3 +614,123 @@ ocnms$year <- year(ocnms$date)
 
 setwd(basewd)
 saveRDS(ocnms, file="data/processed_data/ocnms2_processed.rds")
+
+
+### West Coast Hake Survey
+#2011
+setwd(basewd)
+#Location data
+loc <- read_excel("data/oxygen options/hake/CTD-Location-Files/201103_ctd_locs.xlsx")
+#All the CTD files
+setwd("data/oxygen options/hake/CTD-data/2011-Hake")
+all_files <- list.files(pattern=".csv", recursive=T)
+dats_2011 <- list()
+for(i in 1:length(all_files)){
+  dat <- read.csv(all_files[i])
+  dat$ctd <- substr(all_files[i],1,nchar(all_files[i])-4)
+  loc_i <- as.data.frame(loc[i,])
+  dat <- bind_cols(dat, loc_i)
+  dats_2011[[all_files[i]]] <- dat
+}
+#Combine
+dats_2011 <- bind_rows(dats_2011)
+
+#2012
+setwd(basewd)
+loc <- read_excel("data/oxygen options/hake/CTD-Location-Files/201204_ctd_locs.xlsx")
+setwd("data/oxygen options/hake/CTD-data/2012-Hake")
+all_files <- list.files(pattern=".csv", recursive=T)
+dats_2012 <- list()
+for(i in 1:length(all_files)){
+  dat <- read.csv(all_files[i])
+  dat$ctd <- substr(all_files[i],1,nchar(all_files[i])-4)
+  loc_i <- as.data.frame(loc[i,])
+  dat <- bind_cols(dat, loc_i)
+  dats_2012[[all_files[i]]] <- dat
+}
+
+dats_2012 <- bind_rows(dats_2012)
+
+#2013
+setwd(basewd)
+loc <- read_excel("data/oxygen options/hake/CTD-Location-Files/201305_ctd_locs.xlsx")
+setwd("data/oxygen options/hake/CTD-data/2013-Hake")
+all_files <- list.files(pattern=".csv", recursive=T)
+dats_2013 <- list()
+for(i in 1:length(all_files)){
+  dat <- read.csv(all_files[i])
+  dat$ctd <- substr(all_files[i],1,nchar(all_files[i])-4)
+  loc_i <- as.data.frame(loc[i,])
+  dat <- bind_cols(dat, loc_i)
+  dats_2013[[all_files[i]]] <- dat
+}
+
+dats_2013 <- bind_rows(dats_2013)
+
+#2015
+setwd(basewd)
+loc <- read_excel("data/oxygen options/hake/CTD-Location-Files/201507_ctd_locs.xlsx")
+setwd("data/oxygen options/hake/CTD-data/2015-Hake")
+all_files <- list.files(pattern=".csv", recursive=T)
+dats_2015 <- list()
+for(i in 1:length(all_files)){
+  dat <- read.csv(all_files[i])
+  dat$ctd <- substr(all_files[i],1,nchar(all_files[i])-4)
+  loc_i <- as.data.frame(loc[i,])
+  dat <- bind_cols(dat, loc_i)
+  dats_2015[[all_files[i]]] <- dat
+}
+
+dats_2015 <- bind_rows(dats_2015)
+
+#Combine
+dats2 <- bind_rows(dats_2011, dats_2012, dats_2013, dats_2015)
+
+#Columns of interest
+#Columns needed
+hake <- dats2[,c("T090C", "DepSM", "Sal00", "Sbeox0ML.L", "system (UTC)", "latitude (dec deg)", "longitude(dec deg)")]
+
+#Relabel
+colnames(hake) <- c("temperature_C", "depth", "salinity_psu", "do_mlpL", "date", "latitude", "longitude")
+
+#Get lat and long in UTC coordinates
+hake <- hake %>%
+  st_as_sf(coords=c('longitude','latitude'),crs=4326,remove = F) %>%  
+  st_transform(crs = "+proj=utm +zone=10 +datum=WGS84 +units=km") %>% 
+  mutate(X=st_coordinates(.)[,1],Y=st_coordinates(.)[,2]) 
+
+#sigma and convert O2
+SA = gsw_SA_from_SP(hake$salinity_psu,hake$depth,hake$longitude,hake$latitude) #absolute salinity for pot T calc
+pt = gsw_pt_from_t(SA,hake$temperature_C,hake$depth) #potential temp at a particular depth
+CT = gsw_CT_from_t(SA,hake$temperature_C,hake$depth) #conservative temp
+hake$sigma0_kgm3 = gsw_sigma0(SA,CT)
+hake$O2_umolkg = hake$do_mlpL*44660/(hake$sigma0_kgm3+1000) 
+
+#Survey
+hake$survey <- "hake"
+
+#Type
+hake$type <- "ctd"
+
+#Get date in correct format
+hake$date <- substr(hake$date, 1,10)
+year <- substr(hake$date, nchar(hake$date)-3, nchar(hake$date))
+month <- substr(hake$date, 1,2)
+day <- substr(hake$date,4,5)
+hake$date <- as.POSIXct(as.Date(with(dat,paste(year,month,day,sep="-")),"%Y-%m-%d"))
+
+#DOY
+hake$doy <- as.POSIXlt(hake$date, format = "%Y-%b-%d")$yday
+
+#month and year
+hake$month <- month(hake$date)
+hake$year <- year(hake$date)
+
+#Deepest depth
+hake <- hake %>% group_by(longitude, latitude, date) %>%
+  filter(depth == max(depth)) %>%
+  ungroup() 
+
+#Save
+setwd(basewd)
+saveRDS("data/processed_data/hake_oxygen_processed.rds")
